c0feadf2f9e34e0271fab6f3ec1ec51ed2b5ec77
LifeNock <86575051+LifeNock@users.noreply.github.com>
2026-01-13 13:44:57 -0500

    Update nodes.py

--- src/parser/nodes.py
+++ src/parser/nodes.py
@@ -14,3 +14,18 @@ class BinOpNode:
 
     def __repr__(self):
         return f'({self.left_node}, {self.op_token}, {self.right_node})'
+
+class VarAccessNode:
+    def __init__(self, var_name_token):
+        self.var_name_token = var_name_token
+
+    def __repr__(self):
+        return f'{self.var_name_token}'
+
+class VarAssignNode:
+    def __init__(self, var_name_token, value_node):
+        self.var_name_token = var_name_token
+        self.value_node = value_node
+
+    def __repr__(self):
+        return f'(var {self.var_name_token} = {self.value_node})'
