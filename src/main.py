d2bcf6e1163180bf34694cd6834fb892d63201fd
LifeNock <86575051+LifeNock@users.noreply.github.com>
2026-01-13 13:35:56 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,26 @@
+import sys
+from lexer.lexer import Lexer, TOK_EOF
 
+# --- REPL ---
+def main():
+    while True:
+        try:
+            text = input('Gemstone > ')
+        except EOFError:
+            break
+
+        if not text or text.lower() == 'exit':
+            break
+
+        lexer = Lexer(text)
+
+        try:
+            token = lexer.get_next_token()
+            while token.type != TOK_EOF:
+                print(token)
+                token = lexer.get_next_token()
+        except Exception as e:
+            print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
