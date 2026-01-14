38e983ef70db55390c7bedd489f8434aa6a5684a
LifeNock <86575051+LifeNock@users.noreply.github.com>
2026-01-14 09:58:43 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ import sys
 from lexer.lexer import Lexer, TOK_EOF
 from parser.parser import Parser
 from interpreter.interpreter import Interpreter
+from interpreter.stdlib import load_stdlib
 
 def run(text, interpreter, is_file=False):
-    # 1. Lexer
     lexer = Lexer(text)
     tokens = []
     try:
@@ -12,14 +12,13 @@ def run(text, interpreter, is_file=False):
         while token.type != TOK_EOF:
             tokens.append(token)
             token = lexer.get_next_token()
-        tokens.append(token) # <--- IMPORTANT FIX: Add the EOF token to the list
+        tokens.append(token)
     except Exception as e:
         print(f"Lexer Error: {e}")
         return
 
     if not tokens: return
 
-    # 2. Parser
     parser = Parser(tokens)
     try:
         nodes = parser.parse()
@@ -27,7 +26,6 @@ def run(text, interpreter, is_file=False):
         print(f"Parser Error: {e}")
         return
 
-    # 3. Interpreter
     try:
         for node in nodes:
             result = interpreter.visit(node)
@@ -38,9 +36,9 @@ def run(text, interpreter, is_file=False):
 
 def main():
     interpreter = Interpreter()
+    load_stdlib(interpreter.global_symbol_table)
     
     if len(sys.argv) > 1:
-        # Run File Mode
         filename = sys.argv[1]
         try:
             with open(filename, 'r') as f:
@@ -49,9 +47,7 @@ def main():
         except FileNotFoundError:
             print(f"Could not find file: {filename}")
     else:
-        # Run REPL Mode
-        print("Gemstone Compiler v0.3")
-        print("Type 'exit' to quit.")
+        print("Gemstone Compiler v0.5")
         while True:
             try:
                 text = input('Gemstone > ')
