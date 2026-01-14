df2203db6a7163ede2e6073dab697728ab39c619
LifeNock <86575051+LifeNock@users.noreply.github.com>
2026-01-14 08:40:32 -0500

    Update interpreter.py

--- src/interpreter/interpreter.py
+++ src/interpreter/interpreter.py
@@ -1,10 +1,42 @@
 from lexer.lexer import *
-from parser.nodes import NumberNode, StringNode, BinOpNode, VarAssignNode, VarAccessNode, EmitNode, IfNode
+from parser.nodes import *
+
+# --- INTERPRETER HELPERS ---
+class SymbolTable:
+    def __init__(self, parent=None):
+        self.symbols = {}
+        self.parent = parent
+
+    def get(self, name):
+        value = self.symbols.get(name, None)
+        if value is None and self.parent:
+            return self.parent.get(name)
+        return value
+
+    def set(self, name, value):
+        self.symbols[name] = value
+
+    def remove(self, name):
+        del self.symbols[name]
+
+class Function:
+    def __init__(self, name, body_nodes, arg_names):
+        self.name = name
+        self.body_nodes = body_nodes
+        self.arg_names = arg_names
+
+    def __repr__(self):
+        return f"<function {self.name}>"
+
+class ReturnValue:
+    def __init__(self, value):
+        self.value = value
 
 # --- INTERPRETER ---
 class Interpreter:
     def __init__(self):
-        self.symbol_table = {}
+        self.global_symbol_table = SymbolTable()
+        self.current_symbol_table = self.global_symbol_table
 
     def visit(self, node):
         method_name = f'visit_{type(node).__name__}'
@@ -24,36 +56,26 @@ class Interpreter:
         left = self.visit(node.left_node)
         right = self.visit(node.right_node)
 
-        if node.op_token.type == TOK_PLUS:
-            return left + right
-        elif node.op_token.type == TOK_MINUS:
-            return left - right
-        elif node.op_token.type == TOK_MUL:
-            return left * right
-        elif node.op_token.type == TOK_DIV:
-            return left / right
-        elif node.op_token.type == TOK_EE:
-            return 1 if left == right else 0
-        elif node.op_token.type == TOK_NE:
-            return 1 if left != right else 0
-        elif node.op_token.type == TOK_LT:
-            return 1 if left < right else 0
-        elif node.op_token.type == TOK_GT:
-            return 1 if left > right else 0
-        elif node.op_token.type == TOK_LTE:
-            return 1 if left <= right else 0
-        elif node.op_token.type == TOK_GTE:
-            return 1 if left >= right else 0
+        if node.op_token.type == TOK_PLUS: return left + right
+        elif node.op_token.type == TOK_MINUS: return left - right
+        elif node.op_token.type == TOK_MUL: return left * right
+        elif node.op_token.type == TOK_DIV: return left / right
+        elif node.op_token.type == TOK_EE: return 1 if left == right else 0
+        elif node.op_token.type == TOK_NE: return 1 if left != right else 0
+        elif node.op_token.type == TOK_LT: return 1 if left < right else 0
+        elif node.op_token.type == TOK_GT: return 1 if left > right else 0
+        elif node.op_token.type == TOK_LTE: return 1 if left <= right else 0
+        elif node.op_token.type == TOK_GTE: return 1 if left >= right else 0
 
     def visit_VarAssignNode(self, node):
         var_name = node.var_name_token.value
         value = self.visit(node.value_node)
-        self.symbol_table[var_name] = value
+        self.current_symbol_table.set(var_name, value)
         return value
 
     def visit_VarAccessNode(self, node):
         var_name = node.var_name_token.value
-        value = self.symbol_table.get(var_name)
+        value = self.current_symbol_table.get(var_name)
         if value is None:
             raise Exception(f"'{var_name}' is not defined")
         return value
@@ -64,8 +86,64 @@ class Interpreter:
         return None
 
     def visit_IfNode(self, node):
-        for condition, expr in node.cases:
+        for condition, statement_list in node.cases:
             condition_value = self.visit(condition)
             if condition_value:
-                return self.visit(expr)
+                for stmt in statement_list:
+                    res = self.visit(stmt)
+                    if isinstance(res, ReturnValue): return res
+                return None
+        
+        if node.else_case:
+            for stmt in node.else_case:
+                res = self.visit(stmt)
+                if isinstance(res, ReturnValue): return res
         return None
+
+    def visit_WhileNode(self, node):
+        while True:
+            condition = self.visit(node.condition_node)
+            if not condition: break
+            
+            for stmt in node.body_nodes:
+                res = self.visit(stmt)
+                if isinstance(res, ReturnValue): return res
+        return None
+
+    def visit_FuncDefNode(self, node):
+        func_name = node.var_name_token.value
+        arg_names = [arg.value for arg in node.arg_tokens]
+        func = Function(func_name, node.body_nodes, arg_names)
+        self.current_symbol_table.set(func_name, func)
+        return func
+
+    def visit_FuncCallNode(self, node):
+        function = self.visit(node.node_to_call)
+        
+        args = [self.visit(arg) for arg in node.arg_nodes]
+
+        if len(args) != len(function.arg_names):
+             raise Exception(f"Function {function.name} expects {len(function.arg_names)} args, got {len(args)}")
+
+        new_scope = SymbolTable(parent=self.global_symbol_table)
+        for i in range(len(args)):
+            new_scope.set(function.arg_names[i], args[i])
+
+        previous_scope = self.current_symbol_table
+        self.current_symbol_table = new_scope
+        
+        result = None
+        try:
+            for stmt in function.body_nodes:
+                res = self.visit(stmt)
+                if isinstance(res, ReturnValue):
+                    result = res.value
+                    break
+        finally:
+            self.current_symbol_table = previous_scope
+        
+        return result
+    
+    def visit_ReturnNode(self, node):
+        value = self.visit(node.node_to_return)
+        return ReturnValue(value)
