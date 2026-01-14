aafd68a93f441ebcb786c7b303865a9fc9b39df0
LifeNock <86575051+LifeNock@users.noreply.github.com>
2026-01-14 08:39:44 -0500

    Update lexer.py

--- src/lexer/lexer.py
+++ src/lexer/lexer.py
@@ -1,47 +1,53 @@
 import string
 
-# --- TOKENS ---
-TOK_INT        = 'INT'
-TOK_FLOAT      = 'FLOAT'
-TOK_STRING     = 'STRING'
-TOK_PLUS       = 'PLUS'
-TOK_MINUS      = 'MINUS'
-TOK_MUL        = 'MUL'
-TOK_DIV        = 'DIV'
-TOK_LPAREN     = 'LPAREN'
-TOK_RPAREN     = 'RPAREN'
+# --- CONSTANTS ---
+TOK_INT = 'INT'
+TOK_FLOAT = 'FLOAT'
+TOK_STRING = 'STRING'
+TOK_PLUS = 'PLUS'
+TOK_MINUS = 'MINUS'
+TOK_MUL = 'MUL'
+TOK_DIV = 'DIV'
+TOK_LPAREN = 'LPAREN'
+TOK_RPAREN = 'RPAREN'
+TOK_COMMA = 'COMMA'
 TOK_IDENTIFIER = 'IDENTIFIER'
-TOK_KEYWORD    = 'KEYWORD'
-TOK_EQ         = 'EQ'
-TOK_EE         = 'EE'
-TOK_NE         = 'NE'
-TOK_LT         = 'LT'
-TOK_GT         = 'GT'
-TOK_LTE        = 'LTE'
-TOK_GTE        = 'GTE'
-TOK_EOF        = 'EOF'
+TOK_KEYWORD = 'KEYWORD'
+TOK_EQ = 'EQ'
+TOK_EE = 'EE'
+TOK_NE = 'NE'
+TOK_LT = 'LT'
+TOK_GT = 'GT'
+TOK_LTE = 'LTE'
+TOK_GTE = 'GTE'
+TOK_EOF = 'EOF'
 
 KEYWORDS = [
     'mem',
     'emit',
     'if',
-    'then'
+    'then',
+    'else',
+    'while',
+    'do',
+    'end',
+    'def',
+    'return'
 ]
 
-# --- CLASSES ---
 class Token:
     def __init__(self, type_, value=None):
         self.type = type_
         self.value = value
 
     def __repr__(self):
-        if self.value:
-            return f'{self.type}:{self.value}'
+        if self.value: return f'{self.type}:{self.value}'
         return f'{self.type}'
 
     def matches(self, type_, value):
         return self.type == type_ and self.value == value
 
+# --- LEXER ---
 class Lexer:
     def __init__(self, text):
         self.text = text
@@ -74,7 +80,6 @@ class Lexer:
     def make_number(self):
         num_str = ''
         dot_count = 0
-
         while self.current_char is not None and (self.current_char.isdigit() or self.current_char == '.'):
             if self.current_char == '.':
                 if dot_count == 1: break
@@ -84,19 +89,15 @@ class Lexer:
                 num_str += self.current_char
             self.advance()
 
-        if dot_count == 0:
-            return Token(TOK_INT, int(num_str))
-        else:
-            return Token(TOK_FLOAT, float(num_str))
+        if dot_count == 0: return Token(TOK_INT, int(num_str))
+        else: return Token(TOK_FLOAT, float(num_str))
 
     def make_string(self):
         str_val = ''
         self.advance() 
-
         while self.current_char is not None and self.current_char != '"':
             str_val += self.current_char
             self.advance()
-        
         self.advance()
         return Token(TOK_STRING, str_val)
 
@@ -105,14 +106,12 @@ class Lexer:
         while self.current_char is not None and (self.current_char.isalnum() or self.current_char == '_'):
             id_str += self.current_char
             self.advance()
-
         token_type = TOK_KEYWORD if id_str in KEYWORDS else TOK_IDENTIFIER
         return Token(token_type, id_str)
 
     def make_not_equals(self):
         if self.peek() == '=':
-            self.advance()
-            self.advance()
+            self.advance(); self.advance()
             return Token(TOK_NE)
         self.advance()
         return None 
@@ -143,58 +142,46 @@ class Lexer:
 
     def get_next_token(self):
         while self.current_char is not None:
-
             if self.current_char.isspace():
                 self.skip_whitespace()
                 continue
-
             if self.current_char == '#':
                 self.skip_comment()
                 continue
-
             if self.current_char.isdigit():
                 return self.make_number()
-
             if self.current_char == '"':
                 return self.make_string()
-
             if self.current_char.isalpha():
                 return self.make_identifier()
-
+            if self.current_char == ',':
+                self.advance()
+                return Token(TOK_COMMA)
             if self.current_char == '!':
                 token = self.make_not_equals()
                 if token: return token
                 raise Exception("Expected '!='")
-
             if self.current_char == '=':
                 return self.make_equals()
-
             if self.current_char == '<':
                 return self.make_less_than()
-
             if self.current_char == '>':
                 return self.make_greater_than()
-
             if self.current_char == '+':
                 self.advance()
                 return Token(TOK_PLUS)
-
             if self.current_char == '-':
                 self.advance()
                 return Token(TOK_MINUS)
-
             if self.current_char == '*':
                 self.advance()
                 return Token(TOK_MUL)
-
             if self.current_char == '/':
                 self.advance()
                 return Token(TOK_DIV)
-
             if self.current_char == '(':
                 self.advance()
                 return Token(TOK_LPAREN)
-
             if self.current_char == ')':
                 self.advance()
                 return Token(TOK_RPAREN)
