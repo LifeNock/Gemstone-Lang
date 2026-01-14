cfc189f270688b8df611f08cb1abbce5774a6927
LifeNock <86575051+LifeNock@users.noreply.github.com>
2026-01-14 08:40:16 -0500

    Update parser.py

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,5 +1,5 @@
 from lexer.lexer import *
-from parser.nodes import NumberNode, StringNode, BinOpNode, VarAssignNode, VarAccessNode, EmitNode, IfNode
+from parser.nodes import *
 
 # --- PARSER ---
 class Parser:
@@ -15,6 +15,12 @@ class Parser:
         else:
             self.current_token = Token(TOK_EOF)
         return self.current_token
+
+    def check_token(self, type_):
+        return self.current_token.type == type_
+
+    def check_keyword(self, value):
+        return self.current_token.matches(TOK_KEYWORD, value)
 
     def atom(self):
         token = self.current_token
@@ -37,20 +43,47 @@ class Parser:
             if self.current_token.type == TOK_RPAREN:
                 self.advance()
                 return expr
-            else:
-                raise Exception("Expected ')'")
+            raise Exception("Expected ')'")
         
         raise Exception(f"Expected int, float, string, identifier or '(', found {token}")
 
+    def call(self):
+        atom = self.atom()
+        
+        if self.current_token.type == TOK_LPAREN:
+            self.advance()
+            arg_nodes = []
+            
+            if self.current_token.type == TOK_RPAREN:
+                self.advance()
+            else:
+                arg_nodes.append(self.expr())
+                while self.current_token.type == TOK_COMMA:
+                    self.advance()
+                    arg_nodes.append(self.expr())
+                
+                if self.current_token.type != TOK_RPAREN:
+                    raise Exception("Expected ')'")
+                self.advance()
+            return FuncCallNode(atom, arg_nodes)
+        
+        return atom
+
     def term(self):
-        return self.bin_op(self.atom, (TOK_MUL, TOK_DIV))
+        return self.bin_op(self.call, (TOK_MUL, TOK_DIV))
 
     def arith_expr(self):
         return self.bin_op(self.term, (TOK_PLUS, TOK_MINUS))
 
     def comp_expr(self):
         if self.current_token.matches(TOK_KEYWORD, 'if'):
-            return self.expr()
+            return self.if_expr()
+        if self.current_token.matches(TOK_KEYWORD, 'while'):
+            return self.while_expr()
+        if self.current_token.matches(TOK_KEYWORD, 'def'):
+            return self.func_def()
+        if self.current_token.matches(TOK_KEYWORD, 'return'):
+            return self.return_expr()
             
         node = self.bin_op(self.arith_expr, (TOK_EE, TOK_NE, TOK_LT, TOK_GT, TOK_LTE, TOK_GTE))
         return node
@@ -76,28 +109,99 @@ class Parser:
             expr = self.expr()
             return EmitNode(expr)
 
-        elif self.current_token.matches(TOK_KEYWORD, 'if'):
-            self.advance()
-            condition = self.comp_expr()
-
-            if not self.current_token.matches(TOK_KEYWORD, 'then'):
-                raise Exception(f"Expected 'then', found {self.current_token}")
-
-            self.advance()
-            expr = self.expr()
-            return IfNode([(condition, expr)], None)
-
         return self.comp_expr()
+
+    def block(self):
+        statements = []
+        while not self.check_keyword('end') and not self.check_keyword('else') and not self.check_token(TOK_EOF):
+            statements.append(self.expr())
+        return statements
+
+    def if_expr(self):
+        self.advance()
+        condition = self.comp_expr()
+
+        if not self.check_keyword('then'):
+            raise Exception("Expected 'then'")
+        self.advance()
+
+        true_block = self.block()
+        else_block = None
+
+        if self.check_keyword('else'):
+            self.advance()
+            else_block = self.block()
+
+        if not self.check_keyword('end'):
+             raise Exception("Expected 'end' after if-block")
+        self.advance()
+
+        return IfNode([(condition, true_block)], else_block)
+
+    def while_expr(self):
+        self.advance()
+        condition = self.comp_expr()
+
+        if not self.check_keyword('do'):
+            raise Exception("Expected 'do'")
+        self.advance()
+
+        body = self.block()
+
+        if not self.check_keyword('end'):
+            raise Exception("Expected 'end' after while-block")
+        self.advance()
+
+        return WhileNode(condition, body)
+
+    def func_def(self):
+        self.advance()
+
+        if self.current_token.type != TOK_IDENTIFIER:
+            raise Exception("Expected function name")
+        
+        var_name_token = self.current_token
+        self.advance()
+
+        if self.current_token.type != TOK_LPAREN:
+             raise Exception("Expected '('")
+        self.advance()
+
+        arg_tokens = []
+        if self.current_token.type == TOK_IDENTIFIER:
+            arg_tokens.append(self.current_token)
+            self.advance()
+            while self.current_token.type == TOK_COMMA:
+                self.advance()
+                if self.current_token.type != TOK_IDENTIFIER:
+                    raise Exception("Expected argument name")
+                arg_tokens.append(self.current_token)
+                self.advance()
+        
+        if self.current_token.type != TOK_RPAREN:
+             raise Exception("Expected ')'")
+        self.advance()
+
+        body = self.block()
+
+        if not self.check_keyword('end'):
+             raise Exception("Expected 'end' after function body")
+        self.advance()
+
+        return FuncDefNode(var_name_token, arg_tokens, body)
+    
+    def return_expr(self):
+        self.advance()
+        expr = self.expr()
+        return ReturnNode(expr)
 
     def bin_op(self, func, ops):
         left = func()
-
         while self.current_token.type in ops:
             op_token = self.current_token
             self.advance()
             right = func()
             left = BinOpNode(left, op_token, right)
-
         return left
 
     def parse(self):
