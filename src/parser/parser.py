ced6639834aa1a655bef6ad4f7ad91480bfecd55
LifeNock <86575051+LifeNock@users.noreply.github.com>
2026-01-14 07:30:40 -0500

    Update parser.py

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,5 +1,5 @@
 from lexer.lexer import *
-from parser.nodes import NumberNode, StringNode, BinOpNode, VarAssignNode, VarAccessNode, EmitNode
+from parser.nodes import NumberNode, StringNode, BinOpNode, VarAssignNode, VarAccessNode, EmitNode, IfNode
 
 # --- PARSER ---
 class Parser:
@@ -43,6 +43,16 @@ class Parser:
     def term(self):
         return self.bin_op(self.atom, (TOK_MUL, TOK_DIV))
 
+    def arith_expr(self):
+        return self.bin_op(self.term, (TOK_PLUS, TOK_MINUS))
+
+    def comp_expr(self):
+        if self.current_token.matches(TOK_KEYWORD, 'if'):
+            return self.expr()
+            
+        node = self.bin_op(self.arith_expr, (TOK_EE, TOK_NE, TOK_LT, TOK_GT, TOK_LTE, TOK_GTE))
+        return node
+
     def expr(self):
         if self.current_token.matches(TOK_KEYWORD, 'mem'):
             self.advance()
@@ -64,7 +74,18 @@ class Parser:
             expr = self.expr()
             return EmitNode(expr)
 
-        return self.bin_op(self.term, (TOK_PLUS, TOK_MINUS))
+        elif self.current_token.matches(TOK_KEYWORD, 'if'):
+            self.advance()
+            condition = self.comp_expr()
+
+            if not self.current_token.matches(TOK_KEYWORD, 'then'):
+                raise Exception(f"Expected 'then', found {self.current_token}")
+
+            self.advance()
+            expr = self.expr()
+            return IfNode([(condition, expr)], None)
+
+        return self.comp_expr()
 
     def bin_op(self, func, ops):
         left = func()
